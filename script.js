ff75031c9c09c6d3e8c4bb6f0b4cc0bb4aec50a4
Aayog Koirala <koirala@miamioh.edu>
2023-04-20 16:53:30 +0000

    Apr 20, 2023, 11:53 AM

--- script.js
+++ script.js
@@ -1,29 +1,17 @@
-// Smooth scrolling for internal links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("visible");
+          }
+      });
   });
-});
 
-// Mobile navigation menu
-const nav = document.querySelector('nav');
-const menuIcon = document.createElement('div');
-menuIcon.classList.add('menu-icon');
-menuIcon.innerHTML = '&#9776;';
-
-menuIcon.addEventListener('click', () => {
-  nav.classList.toggle('mobile-nav');
-});
-
-nav.insertBefore(menuIcon, nav.firstChild);
-
-// Hide mobile navigation menu when clicking a link
-nav.addEventListener('click', (e) => {
-  if (e.target.tagName === 'A') {
-    nav.classList.remove('mobile-nav');
-  }
+  document.querySelectorAll("section").forEach((section) => {
+      section.style.opacity = "0";
+      section.querySelectorAll("h2, h3, p, ul, figure").forEach((el) => {
+          el.style.opacity = "0";
+      });
+      observer.observe(section);
+  });
 });
